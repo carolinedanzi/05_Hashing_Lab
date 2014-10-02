36a526966a4d50ef5cfba64faee9e7d1805d61d7
unknown <danzicr@EAS-BEN006-C1.it.muohio.edu>
2014-10-02 12:51:57 -0400

    Class notes for October 2nd.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "hashPrimes.h"
-#include "HashTable.h"
 #include <string>
+// need to declare it before use in HashTable.h
+unsigned long hash(std::string k);
+#include "HashTable.h"
 #include <iostream>
 #include <sstream>
 
@@ -15,7 +17,7 @@
 unsigned long hash(std::string k){
   unsigned long m = hashPrimes[NUM_HASH_PRIMES-2];
   unsigned long ret = 0;
-  for(int i=0;i<k.size();i++){
+  for(unsigned int i=0;i<k.size();i++){
     ret = (256*ret + k[i])%m;
   }
   return ret;
@@ -162,7 +164,7 @@ void testGrow(){
     return;
   }
 
-  for(int j=0; j<=i;j++){
+  for(unsigned int j=0; j<=i;j++){
     std::ostringstream ss;
     ss << j;
     if(!testHash.keyExists(ss.str())){
